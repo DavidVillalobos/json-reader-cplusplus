71cef0b03918058d4cf53dc65b9f66d9322a8921
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-08 04:06:34 -0600

    create main and makefile

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#include"Simple.h"
+#include"Composite.h"
+
+int main(int argc, char** argv){
+	Composite v("person", "object");
+	string name = "age";
+	v.properties.insert({name, new Simple<int>(name, 21)});
+	Property* f = v.properties[name];
+	if(f->getType() == "int"){
+		Simple<int> *c = dynamic_cast<Simple<int>*>(f);
+		int val = c->getValue();
+		cout << "The val is " << val << endl; 
+	}
+}
