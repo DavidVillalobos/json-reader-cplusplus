a460e022d44521d3208ad2bf1ed2e6fa289720a6
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-15 23:35:06 -0600

    update json

--- src/Json.cpp
+++ src/Json.cpp
@@ -1,18 +1,47 @@
 /*
     File: Json.cpp
     Author: David Villalobos
-    Date: 2021-25-03
+    Date: 2021-12-03
     Description: Implementation of class Json
-    to reader a file json and create a structure.
+    to represent a object or an array.
 */
 #include"Json.h"
 
-Json::Json(std::string path){
+Json::Json(std::string name, std::string value) : Element(name, value){
+
+}
+
+Element* Json::getObject(int index){
+    return value[std::to_string(index)];
+}
+
+Element* Json::getProperty(std::string prop){
+    return value[prop];
+}
+
+std::ostream& operator << (std::ostream &o,const Json &c)
+{
+    for () {
+
+    }
+    o << c.value;
+    return o;
+}
+
+Json::~Json(){
+    for (auto x : value){
+        delete x.second; // free memory
+    }
+}
+
+
+
+Json::Json(std::string path) {
     this->path = path;
-    this->regex_properties_in_obj = std::regex("\"([\\w0-9. ]+)\":\"?([\\w0-9. ]+|\\[[\\w0-9. \"{}:,]+\\]|\\{[\\w0-9 .\"{}:,]+\\})\"?");
-    this->regex_load_path_prop = std::regex("([\\w.'_-]+)/?");
+    this->regex_value_in_obj = std::regex("\"([\\w0-9. ]+)\":\"?([\\w0-9. ]+|\\[[\\w0-9. \"{}:,]+\\]|\\{[\\w0-9 .\"{}:,]+\\})\"?");
     // Bugs when debbuger (gdb) maybe not locate a file :0
-    // Read a json file  /*
+    // Read a json file  
+    /*
     std::ifstream f(path);
     if(f.good()){
         std::string aux;
@@ -24,96 +53,99 @@ Json::Json(std::string path){
         throw "FAIL: File not found or not load in path" + path;
     }
     // */
-    //file = "{\"firstName\":\"Rack\",\"lastName\":\"Jackon\",\"gender\":\"man\",\"age\":24,\"address\":{\"streetAddress\":\"126\",\"city\":\"SanJone\",\"state\":\"CA\",\"postalCode\":\"394221\"},\"phoneNumbers\":[{\"type\":\"home\",\"number\":\"7383627627\",\"example\":{\"object\":\"name\",\"value\":false}},{\"type\":\"office\",\"number\":\"8462945527\"}]}";
+    file = "{\"firstName\":\"Rack\",\"lastName\":\"Jackon\",\"gender\":\"man\",\"age\":24,\"address\":{\"streetAddress\":\"126\",\"city\":\"SanJone\",\"state\":\"CA\",\"postalCode\":\"394221\"},\"phoneNumbers\":[{\"type\":\"home\",\"number\":\"7383627627\",\"example\":{\"object\":\"name\",\"value\":false}},{\"type\":\"office\",\"number\":\"8462945527\"}]}";
     // Remove endlines, tabs and black spaces (not in string)
-    std::regex r("\n|\t| (?=([^\"]*\"[^\"]*\")*[^\"]*$)"); 
+    std::regex r("\n|\t| (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
     file = std::regex_replace(file, r, "");
-    this->object = loadPropertiesFromObject("root", file);
+    this->object = loadvalueFromObject("root", file);
 }
 
-Json::~Json(){
-    delete object;
-}
-
-std::string Json::getObjectFromArray(std::string &arr){
+std::string Json::getObjectFromArray(std::string& arr) {
     std::string result = "";
     char simbol;
     int cant_curly_brackets = 1;
-    arr.erase(0,1); // remove first '{'
-    while(true){
+    arr.erase(0, 1); // remove first '{'
+    while (true) {
         simbol = arr[0];
-        arr.erase(0,1);
-        if(simbol == '{'){ cant_curly_brackets++; }
-        else if(simbol == '}'){ cant_curly_brackets--; }
-        if(cant_curly_brackets == 0){ break; }
+        arr.erase(0, 1);
+        if (simbol == '{') { cant_curly_brackets++; }
+        else if (simbol == '}') { cant_curly_brackets--; }
+        if (cant_curly_brackets == 0) { break; }
         result += simbol;
     }
     // Remove a residue
-    while(arr[0] == ' ' || arr[0] == ',') arr.erase(0,1);
+    while (arr[0] == ' ' || arr[0] == ',') arr.erase(0, 1);
     return result;
-} 
+}
 
-Composite* Json::loadObjectsFromArray(std::string arrName, std::string arrBody){
-    Composite* obj = new Composite(arrName, arrBody);
-    arrBody.erase(0,1); // remove first '['
-    arrBody.erase(arrBody.length()-1, 1); // remove first '['
+Json* Json::loadObjectsFromArray(std::string arrName, std::string arrBody) {
+    Json* obj = new Json(arrName, arrBody);
+    arrBody.erase(0, 1); // remove first '['
+    arrBody.erase(arrBody.length() - 1, 1); // remove first '['
     int pos = 0;
     while (!arrBody.empty()) {
-        obj->properties[std::to_string(pos)] = loadPropertiesFromObject(std::to_string(pos), getObjectFromArray(arrBody));
+        obj->value[std::to_string(pos)] = loadvalueFromObject(std::to_string(pos), getObjectFromArray(arrBody));
         pos++;
     }
     return obj;
 }
 
-Composite* Json::loadPropertiesFromObject(std::string objName, std::string objBody){
-    Composite* obj = new Composite(objName, objBody);
+Json* Json::loadvalueFromObject(std::string objName, std::string objBody) {
+    Json* obj = new Json(objName, objBody);
     std::string name, value;
     std::smatch match;
-    while (std::regex_search(objBody, match, regex_properties_in_obj)) {
-        std::smatch::iterator i = match.begin(); 
+    while (std::regex_search(objBody, match, regex_value_in_obj)) {
+        std::smatch::iterator i = match.begin();
         i++;
         name = i->str();
-        i++;        
-        value = i->str();      
-        if(i->str()[0] == '{'){// is other object
-            obj->properties[name] = loadPropertiesFromObject(name, value);
-        } else if(i->str()[0] == '['){// is array
-            obj->properties[name] = loadObjectsFromArray(name, value);
-        }else{// is simple property
-            obj->properties[name] = new Property(name,value);
+        i++;
+        value = i->str();
+        if (i->str()[0] == '{') {// is other object
+            obj->value[name] = loadvalueFromObject(name, value);
+        }
+        else if (i->str()[0] == '[') {// is array
+            obj->value[name] = loadObjectsFromArray(name, value);
+        }
+        else {// is simple property
+            obj->value[name] = new Property(name, value);
         }
         objBody = match.suffix().str(); // extract groups finded in objBody
     }
-    return obj; 
+    return obj;
 }
 
-std::string Json::getFile(){
+std::string Json::getFile() {
     return file;
 }
 
-std::string Json::getPath(){
+std::string Json::getPath() {
     return path;
 }
 
-std::string Json::operator[](std::string path_prop){
-    std::smatch match;
-    Element* e = object; 
-    std::string res = "";
-    std::string prop = "";
-    int index;
-    while (regex_search(path_prop, match, regex_load_path_prop)) {
-        std::smatch::iterator i = match.begin();
-        i++;
-        prop = i->str();
-        try{
-            index = std::stoi(prop);// is array
-            e = ((Composite*)e)->getObject(index); 
-        }catch(...){ // is object or simple property
-            e =  ((Composite*)e)->getProperty(prop);
-        }
-        if(e) res = e->getValue();
-        else return ""; // raise exception
-        path_prop = match.suffix().str();
-    } 
-    return res;
+Json::operator std::string() {
+    return "GET STRING PROPERTY";
+}
+
+Json& Json::operator[](const char* prop) {
+    return getProperty(std::string(prop));
+}
+
+Json& Json::operator[](std::string prop) {
+    return getProperty(prop);
+}
+
+Json& Json::operator[](int index)
+{
+    return getProperty(std::to_string(index));
+}
+
+Json& Json::getProperty(std::string prop) {
+    Element* e = value[prop];
+    Property* p = dynamic_cast<Property*>(e);
+    if (p) {
+
+    }
+    else {
+        Json* j = dynamic_cast<Json*>(e);
+    }
 }
--- src/Json.h
+++ src/Json.h
@@ -1,36 +1,42 @@
 /*
-    File: Json.h
+    File: Composite.h
     Author: David Villalobos
-    Date: 2021-24-03
-    Description: Declaration of class Json
-    to reader a file json and create a structure.
+    Date: 2021-12-03
+    Description: Declaration of class Composite
+    to represent a object or an array.
 */
-#ifndef JSON_H
-#define JSON_H
 
-//Includes
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
+
+// Includes
+#include <map>
 #include <regex> 
 #include<fstream>
 
-#include"Composite.h"
 #include"Property.h"
 
-
-class Json{
+class Json : public Element{
     private:
+        std::map<std::string, Element*> value;
         std::string path;
         std::string file;
-        std::regex regex_properties_in_obj; 
-        std::regex regex_load_path_prop;
-        Composite* loadObjectsFromArray(std::string arrName, std::string arrBody);
-        Composite* loadPropertiesFromObject(std::string objName, std::string objBody);
+        Json* loadObjectsFromArray(std::string arrName, std::string arrBody);
+        Json* loadPropertiesFromObject(std::string objName, std::string objBody);
+        std::string getObjectFromArray(std::string& arr);
+        Json& getProperty(std::string prop);
     public:
-        Composite* object;
         Json(std::string path);
-        std::string getObjectFromArray(std::string &arr);
+        Json(std::string name, std::string value);
+        friend std::ostream& operator <<(std::ostream &o, const Json &c)
+
         ~Json();
         std::string getFile();
         std::string getPath();
-        std::string operator [](std::string path_prop);
+        operator std::string();
+        Json& operator[](const char* prop);
+        Json& operator[](std::string prop);
+        Json& operator[](int index);
 };
-#endif // !JSON_H
+
+#endif // !COMPOSITE_H
