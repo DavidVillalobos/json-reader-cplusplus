1737db427d155e51a5b254ce05241a68a12d24af
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-15 23:35:23 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,25 +20,28 @@ int main(int argc, char** argv){
 	// Test example.json
 	std::cout << "Test for " << file_1.getPath() << std::endl;
 	// TEST #1
-	execute_test(file_1, number_test++, "Name", "Denu");
+	//execute_test(file_1, number_test++, "Name", "Denu");
 	// TEST #2
-	execute_test(file_1, number_test++, "Contact", "12345678");
+	//execute_test(file_1, number_test++, "Contact", "12345678");
 	number_test = 1;
 	// Test example2.json
 	std::cout << "Test for " << file_2.getPath() << std::endl;
 	// TEST #1
-	execute_test(file_2, number_test++, "firstName", "Rack");
+	//execute_test(file_2, number_test++, "firstName", "Rack");
 	// TEST #2
-	execute_test(file_2, number_test++, "address/city", "San Jone");
+	//execute_test(file_2, number_test++, "address/city", "San Jone");
 	// TEST #3
-	execute_test(file_2, number_test++, "address/postalCode", "394221");
+	//execute_test(file_2, number_test++, "address/postalCode", "394221");
 	// TEST #4
-	execute_test(file_2, number_test++, "address/streetAddress", "126");
+	//execute_test(file_2, number_test++, "address/streetAddress", "126");
 	// TEST #5
-	execute_test(file_2, number_test++, "phoneNumbers/1/number", "8462945527");
+	execute_test(file_2, number_test++, "firstName", "8462945527");
+	std::cin.get();
 }
 
-void execute_test(Json& file, int number, std::string path, std::string val_expected){ 
-	std::cout << "Test #" << number << " -> " << ((file[path] == val_expected)? " Pass :D" : " Fail :c") << '\n';
+void execute_test(Json& file, int number, std::string path, std::string val_expected){
+	std::string result = file[path];
+	std::cout << "Test #" << number << " -> " << result << std::endl;
+	//std::cout << "Test #" << number << " -> " << ((result == val_expected)? " Pass :D" : " Fail :c") << '\n';
 }
 
