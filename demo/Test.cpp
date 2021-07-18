b31d5bce20c12c149cf845a1762124109d22c0c4
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-18 03:28:01 -0600

    refactor: update demo

--- demo/Test.cpp
+++ demo/Test.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-#include"JsonReader.h" // include this file
+#include"../src/JsonReader.h" // include this file
 
 int main(int argc, char** argv){
     // build a Object Json
 	Json file("file.json"); // path to json file
     // Get properties like -> file["path_to_property"]
     cout << "The name is " << file["firstName"] << ' ' << file["lastName"] << endl;
-    cout << "The city is " << file["address/city"] << endl;
-    cout << "The phone office is " << file["phoneNumbers/1/number"] << endl;
+    cout << "The city is " << file["address"]["city"] << ", " << file["address"]["state"] << endl;
+    cout << "The phone office is " << file["phoneNumbers"][1]["number"] << endl;
 }
